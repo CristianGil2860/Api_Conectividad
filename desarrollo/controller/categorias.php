d8105a1f7b51fb6dc9e8e44c9fd205c38a6e203b
macorrea1 <correamarcositec@gmail.com>
2022-08-20 15:25:55 -0300

    paginador.php

--- desarrollo/controller/categorias.php
+++ desarrollo/controller/categorias.php
@@ -36,10 +36,25 @@ $contador8 = $contadorcase8->rowCount();
 $sumatelecom = $contador3 + $contador2 + $contador1;
 $sumacordinacion = $contador7 + $contador8 + $contador6;
 $sumaeducar = $contador5 + $contador4;
-/////////////////////////////////////////////
+/////////////////////////////////////////////paginacion
+//limit por pagina
+$incxpagina = 40;
+// $limit = $incxpagina;
+$pagina = '';
+// aca es que tomamos lo  que nos envi
+if (isset($_POST['pagina'])) {
+  $pagina = $_POST['pagina'];
+} else {
+  $pagina = 1;
+}
+$offset = $pagina  * $incxpagina;
+$star_from = ($pagina - 1) * $incxpagina;
 
+/////
+
+////
 $categorias = isset($_POST['categorias']) ? $_POST['categorias'] : "";
-
+// header('Content-type: application/json; charset=utf-8');
 
 switch ($categorias) {
   case 0:
@@ -48,7 +63,10 @@ switch ($categorias) {
 
     //inner join base con_sit
 
-    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores ORDER BY movimientos.incidencia_id ");
+    $consultsit1 = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
+    $consultsit1->execute();
+
+    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
     $consultsit->execute();
     $tablasit = $consultsit->fetchAll(PDO::FETCH_ASSOC);
 
@@ -57,49 +75,62 @@ switch ($categorias) {
 
     $provedores = 6;
 
+    $consultsit1 = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
+    $consultsit1->execute();
     //inner join base con_sit
-    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores ORDER BY movimientos.incidencia_id ");
+    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimiento ");
     $consultsit->execute();
     $tablasit = $consultsit->fetchAll(PDO::FETCH_ASSOC);
+
     break;
   case 2:
 
     $provedores = 8;
 
+    $consultsit1 = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
+    $consultsit1->execute();
     //inner join base con_sit
-    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores ORDER BY movimientos.incidencia_id ");
+    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
     $consultsit->execute();
     $tablasit = $consultsit->fetchAll(PDO::FETCH_ASSOC);
+
     break;
   case 3:
-
+    //traslados en linea
     $provedores = 3;
 
+    $consultsit1 = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
+    $consultsit1->execute();
     //inner join base con_sit
-    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores ORDER BY movimientos.incidencia_id ");
+    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
     $consultsit->execute();
     $tablasit = $consultsit->fetchAll(PDO::FETCH_ASSOC);
-    
+
+
 
     break;
 
-
+    //pedidos en linea
   case 4:
     $provedores = 2;
 
-    //inner join base con_sit
-    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores ORDER BY movimientos.incidencia_id ");
+    $consultsit1 = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
+    $consultsit1->execute();
+
+    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) LIMIT $star_from,$incxpagina ");
     $consultsit->execute();
     $tablasit = $consultsit->fetchAll(PDO::FETCH_ASSOC);
 
     break;
-
+    //reparacion linea
   case 5:
     $provedores = 1;
 
+    $consultsit1 = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
+    $consultsit1->execute();
 
     //inner join base con_sit
-    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores ORDER BY movimientos.incidencia_id ");
+    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) LIMIT $star_from,$incxpagina ");
     $consultsit->execute();
     $tablasit = $consultsit->fetchAll(PDO::FETCH_ASSOC);
 
@@ -109,20 +140,36 @@ switch ($categorias) {
   case 6:
     $provedores = 4;
 
+    $consultsit1 = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
+    $consultsit1->execute();
 
     //inner join base con_sit
-    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores ORDER BY movimientos.incidencia_id ");
+    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
     $consultsit->execute();
     $tablasit = $consultsit->fetchAll(PDO::FETCH_ASSOC);
+
     break;
   case 7:
     $provedores = 5;
 
 
     //inner join base con_sit
-    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores ORDER BY movimientos.incidencia_id ");
+    //obtener paginas 
+    $consultsit1 = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
+    $consultsit1->execute();
+    //obtener productos
+    $consultsit = $conexionsit->prepare("SELECT incidencias.id_escuela,incidencias.ticket_itop,movimientos.fecha_mod,movimientos.incidencia_id,movimientos.id,estados.descripcion FROM movimientos  INNER JOIN incidencias INNER JOIN estados ON  movimientos.estado_id=estados.id AND incidencias.id=movimientos.incidencia_id  WHERE incidencias.incidenciaPorProveedor_id=$provedores AND movimientos.id IN (SELECT MAX(movimientos.id) FROM movimientos GROUP BY movimientos.incidencia_id) ");
     $consultsit->execute();
     $tablasit = $consultsit->fetchAll(PDO::FETCH_ASSOC);
+
     break;
 }
 
+
+//debo contar las filas que trae el $tablasit
+$totalincidencias = $consultsit1->rowCount();
+// var_dump($totalincidencias);
+$paginas = $totalincidencias / $incxpagina;
+$paginas = ceil($paginas);
+//en este var_dump para poder resultado entonces el float debemos siempre redondear para arriba "ceil"
+// var_dump($paginas);
