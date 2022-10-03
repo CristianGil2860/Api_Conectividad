c0216d070340139fd63e3fc1cd45de3c65e8d9a8
macorrea1 <correamarcositec@gmail.com>
2022-10-02 21:19:56 -0300

    validacion del loginjs

--- desarrollo/controller/validationlogin.php
+++ desarrollo/controller/validationlogin.php
@@ -1,14 +1,15 @@
-<?php 
+<?php
+
 session_start();
-if(isset($_SESSION['nombredeluser']))
-{
+if (isset($_SESSION['nombredeluser'])) {
     header('location:../');
 }
 include("../config/database.php");
-$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
-$pass  = isset($_POST['pass']) ? $_POST['pass'] : '';
+// $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+// $pass  = isset($_POST['pass']) ? $_POST['pass'] : '';
+// header('Content-Type: application/json');
 if (isset($_POST['nombre_usuario'], $_POST['contrasena'])) {
-    //declaro variables recibidas por post
+    //declaro variables recibidas por post  
     $usuario = $_POST['nombre_usuario'];
     $contra = $_POST['contrasena'];
     //ejecuto consulta a la tabla usuarios
@@ -16,19 +17,24 @@ if (isset($_POST['nombre_usuario'], $_POST['contrasena'])) {
 
     $sql->execute();
     $usuariocont = $sql->fetch(PDO::FETCH_LAZY);
+
     //comparo los campos ingresados del login
-    if ($usuario === $usuariocont['nombre_usuario'] and $contra === $usuariocont['contrasena']) {
-        $_SESSION['nombredeluser']=$usuario;
-        header('Location:../');
-    } else if ($usuario !== $usuariocont['nombre_usuario'] and $contra !== $usuariocont['contrasena']) {
-        header('location:../');
-        
+    if ($usuariocont == null) {
+        // header('location:../');
+        echo json_encode(array('data' => 0));
+    } else if ($usuariocont !== null) {
+        $_SESSION['nombredeluser'] = $usuario;
+        echo json_encode(array('data' => 1, 'user' => $usuario));
     }
-    
+    exit();
 }
+
+
+//cerrar sesion 
+$botoncerrarsesion = ($_POST['btncerrarsesion']);
+var_dump($botoncerrarsesion);
 if (isset($_POST['btncerrarsesion'])) {
+
     session_destroy();
     header('location:../');
 }
-
-?>
